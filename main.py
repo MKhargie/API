6355731632eba5e028e90b08bd437e1d65562aeb
Matt Khargie <mattkhargie@gmail.com>
2024-11-01 12:18:19 -0400

    post

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    input: str
 
 @app.get("/")
 async def root():
     return {"message": "Hi Matt"}
+
+@app.post("/submit/")
+async def submit(item: Item):
+    return {"message": f"{item.input} success"}
